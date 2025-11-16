ea6d425849624768803895342fc063b63102d386
Ziad-Belal <ziadbelal82@gmail.com>
2025-11-16 19:52:38 +0200

    firslty, I added the two CSVs the model will learn on and connected it to the model

---
+++ Project.py
@@ -0,0 +1,6 @@
+import pandas as panda
+All_players=panda.read_csv('All_Players.csv')
+season=panda.read_csv('Season.csv')
+print(All_players.head())# to make sure the csv is connected to the model and that the modl have seen it
+
+
